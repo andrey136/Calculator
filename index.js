395f744a1aecb652a3fec9d7a848b7a559051b9e
Andrey Mardash <andrey.mardash@gmail.com>
2018-08-02 19:12:44 +0300

    great

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 console.log('start');
 document.body.style.backgroundColor = '#'+Math.floor(Math.random()*16777215).toString(16);
 const printAndAddNumber = function (n) {
+    if (str === arr[0]) clearAmount();
     console.log(arr);
-    if (str[str.length - 1] === '.' && n === '.') return str;
     str += n;
     if (str === '00') return str = '0';
     if (firstNumber) return document.getElementById('amount').value = str;
@@ -11,17 +11,15 @@ const printAndAddNumber = function (n) {
 const buttonAmount1 = function (n) {
 
     if (operationWithResult){
-        //console.log
+        if (str === '' && arr3.includes(arr[arr.length - 1])) {
+           return diffrentSigns(n);
+        }
         str = '';
         arr.push(n);
         return document.getElementById('amount').value += n;
     }
     if (str === '' && arr3.includes(arr[arr.length - 1])) {
-        arr[arr.length - 1] = n;
-        for (let i = 0; i < arr.length - 1; i++) {
-            document.getElementById('amount').value = arr[i];
-        }
-        return document.getElementById('amount').value += n;
+       return diffrentSigns(n);
     }
     if (str !== '') {
         arr.push(str, n);
@@ -40,6 +38,7 @@ const buttonAmount2 = function (n) {
     console.log(arr);
     str = '';
     count = +arr[0];
+    console.log(arr[2]);
     for (let i = 1; i < arr.length - 1; i += 2) {
         if (arr[i] === '+') count += +arr[i + 1];
         if (arr[i] === '-') count -= +arr[i + 1];
@@ -63,6 +62,24 @@ const clearAmount = function () {
     console.log(arr);
     firstNumber = true;
     operationWithResult = false;
+}
+const point = function () {
+    if (str === arr[0]) return arr;
+    if (str[str.length - 1] === '.') return str;
+    if (str === '' && typeof arr[0] === 'undefined'){
+        str += '0.';
+        return document.getElementById('amount').value = '0.'
+    }
+    if (str === '') return str;
+    str += '.';
+    document.getElementById('amount').value += '.';
+}
+function diffrentSigns (n) {
+    arr[arr.length - 1] = n;
+    for (let i = 0; i < arr.length - 1; i++) {
+        document.getElementById('amount').value = arr[i];
+    }
+    return document.getElementById('amount').value += n;
 }
 let arr = [];
 let str = '';
